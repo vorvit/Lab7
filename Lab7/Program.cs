b94c575d7e71f10aeb5d357b1ac32621107efda7
vorvit <voronikv_home@mail.ru>
2021-11-29 23:42:52 +0300

    Добавьте файлы проекта.

---
+++ Lab7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 7-1. О большей площади двух треугольников
+            double s;
+            Console.WriteLine("Введите длины сторон первого треугольника:");
+            Console.WriteLine("сторона А:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("сторона B:"); 
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("сторона С:"); 
+            double c = Convert.ToDouble(Console.ReadLine());
+            CalcTriangleSquare(a, b, c, out s);
+            double s1 = s;
+            Console.WriteLine("Введите длины сторон второго треугольника:");
+            Console.WriteLine("сторона А:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("сторона B:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("сторона С:");
+            c = Convert.ToDouble(Console.ReadLine());
+            CalcTriangleSquare(a, b, c, out s);
+            double s2 = s;
+            if (s1 > s2)
+            {
+                Console.WriteLine("Площадь первого треугольника S1 = {0}, больше площади второго S2 = {1}", s1, s2);
+            }
+            if (s1 < s2)
+            {
+                Console.WriteLine("Площадь второго треугольника S2 = {0}, больше площади первого S1 = {1}", s2, s1);
+            }
+            if (s1 == s2)
+            {
+                Console.WriteLine("Площади треугольников равны S1 = S2 = {0}", s1);
+            }
+            Console.WriteLine("Для завешения - нажмите любую клавишу");
+            Console.ReadKey();
+        }
+        static void CalcTriangleSquare(double a, double b, double c, out double s)
+        {
+            double hp = (a + b + c) / 2;
+            s = Math.Sqrt(hp * (hp - a) * (hp - b) * (hp - c));
+        }
+    }
+}
---
+++ Task_7-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Задача 7-2. О методе вычисления объема и площади поверхности куба по длине его ребра
+            double s, v;
+            Console.WriteLine("Введите длину ребра куба:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            CalcCube(a, out s, out v);
+            Console.WriteLine("Площадь поверхности граней куба S = {0}", s);
+            Console.WriteLine("Объем куба V = {0}", v);
+            Console.WriteLine("Для завешения - нажмите любую клавишу");
+            Console.ReadKey();
+        }
+        static void CalcCube(double a, out double s, out double v)
+        {
+            s = 6 * a * a;
+            v = a * a * a;
+        }
+    }
+}
